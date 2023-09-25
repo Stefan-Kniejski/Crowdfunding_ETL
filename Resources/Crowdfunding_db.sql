af5e4b1101afec087a1041a7b856acc3672657ad
rosamp1 <132237292+rosamp1@users.noreply.github.com>
2023-09-25 18:38:10 -0500

    final commit

---
+++ Resources/Crowdfunding_db.sql
@@ -0,0 +1,15 @@
+ALTER TABLE campaign
+ALTER COLUMN goal TYPE numeric;
+
+ALTER TABLE campaign
+ALTER COLUMN pledged TYPE numeric;
+
+
+SELECT * from campaign
+
+SELECT * from contacts
+
+SELECT * from category
+
+SELECT * from subcategory
+
